f286955505cb4334f4a9974191a11199eea5a594
arnoldoJose <aolivasgonzales73@gmail.com>
2021-05-10 18:08:21 -0600

    Updates

---
+++ precache-manifest.e6481392e1f734dc4e10cf7c45d39dd5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "629eda02a28ab7ee4cd1",
+    "url": "/sistemAdministration/static/css/main.da082f89.chunk.css"
+  },
+  {
+    "revision": "629eda02a28ab7ee4cd1",
+    "url": "/sistemAdministration/static/js/main.90150c99.chunk.js"
+  },
+  {
+    "revision": "5bd808ef9e0f05220bee",
+    "url": "/sistemAdministration/static/js/runtime~main.12448687.js"
+  },
+  {
+    "revision": "08154009c6b24442fcf3",
+    "url": "/sistemAdministration/static/css/2.ca4a3cb9.chunk.css"
+  },
+  {
+    "revision": "08154009c6b24442fcf3",
+    "url": "/sistemAdministration/static/js/2.64eec97f.chunk.js"
+  },
+  {
+    "revision": "358dd2d793c4d0ef772a",
+    "url": "/sistemAdministration/static/js/3.f9f6fcad.chunk.js"
+  },
+  {
+    "revision": "31d006a6cd9a75720e64c0c405744425",
+    "url": "/sistemAdministration/static/media/logo.31d006a6.png"
+  },
+  {
+    "revision": "e27c8735da98ec6ccdcf12e258b26475",
+    "url": "/sistemAdministration/static/media/user.e27c8735.png"
+  },
+  {
+    "revision": "0ca98176dcada8c700737cfe0fc73574",
+    "url": "/sistemAdministration/static/media/book.0ca98176.png"
+  },
+  {
+    "revision": "a975db007fe4b2251bda98349d2c1379",
+    "url": "/sistemAdministration/static/media/calendar.a975db00.png"
+  },
+  {
+    "revision": "2630c1572a43a1ba4d874cb5187ea98e",
+    "url": "/sistemAdministration/static/media/fondoL.2630c157.jpg"
+  },
+  {
+    "revision": "3cde28552a4ff59dbda795adf81403b9",
+    "url": "/sistemAdministration/index.html"
+  }
+];
